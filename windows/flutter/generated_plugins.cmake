b839fc0f697a2629ad16f456d0f408b815630ea8
Азизбек Вахидов <vaxidov.aziz@loc.tashkent.uz>
2022-02-07 01:12:09 +0500

    add some package settings for windows

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,8 +3,11 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  bitsdojo_window_windows
+  flutter_acrylic
   network_info_plus_windows
   printing
+  system_theme
   url_launcher_windows
   window_manager
 )
